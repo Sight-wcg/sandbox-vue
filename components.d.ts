1ff7478821791c0bf2607ad273f5d4738cf13b1e
sight <1453017105@qq.com>
2022-02-06 07:10:14 +0800

    chore: 切换 pnpm

--- components.d.ts
+++ components.d.ts
@@ -5,7 +5,6 @@
 declare module 'vue' {
   export interface GlobalComponents {
     Header: typeof import('./src/components/Header.vue')['default']
-    LayLayer: typeof import('@layui/layui-vue/lib/layui-vue.es.js')['LayLayer']
   }
 }
 
