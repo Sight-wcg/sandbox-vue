5b89d7d0b5088fe746765f0ca354d1267810b196
sight <1453017105@qq.com>
2022-02-22 21:28:41 +0800

    chore: 测试目前支持的组件库

--- src/config/sandbox.config.ts
+++ src/config/sandbox.config.ts
@@ -23,11 +23,23 @@ export interface Dependency {
 export default {
 
 }
-
+/**
+ * 支持以下组件库
+ * @layui-vue
+ * element-plus
+ * vant
+ * quasar
+ */
 export const config = {
   title: 'sandbox-vue',
+  //UIPackage: 'ant-design-vue',
   UIPackage: '@layui/layui-vue',
   //UIPackage: 'element-plus',
+  //UIPackage: 'naive-ui',
+  //UIPackage: 'vant',
+  //UIPackage: 'tdesign-vue-next',
+  //UIPackage: '@arco-design/web-vue',
+  //UIPackage: 'quasar',
   minSupportedVersion: '0.2.5',
   filterPreRelease: false,
   defaultAppTemplate: defaultAppTemplate.trim()
@@ -84,7 +96,7 @@ export const dependencies: Dependency[] = [
   {
     name: '@layui/layer-vue',
     version: '',
-    path: '/lib/layer-vue.es.min.js',
+    path: '/lib/layer-vue.es.js',
     stylePath: '',
     description: 'layer弹层',
     source: 'jsdelivr',
@@ -95,6 +107,76 @@ export const dependencies: Dependency[] = [
     path: '/dist/index.full.mjs',
     stylePath: '/dist/index.css',
     description: 'element plus 组件库',
+    source: 'unpkg',
+  },
+
+  // 无默认导出,Store 中 { Quasar } 引入
+  {
+    name: 'quasar',
+    version: '',
+    path: '/dist/quasar.esm.prod.js',
+    stylePath: '/dist/quasar.prod.css',
+    description: 'quasar 组件库',
+    source: 'unpkg',
+  },
+
+  // {
+  //   name: '@arco-design/web-vue',
+  //   version: '',
+  //   path: '/es/index.js',
+  //   stylePath: '/dist/arco.css',
+  //   description: 'arco-design 组件库',
+  //   source: 'unpkg',
+  // },
+
+  // {
+  //   name: 'tdesign-vue-next',
+  //   version: '',
+  //   path: '/es/index.js',
+  //   stylePath: '/dist/tdesign.css',
+  //   description: 'tdesign-vue-next 组件库',
+  //   source: 'unpkg',
+  // },
+
+  // {
+  //   name: 'ant-design-vue',
+  //   version: '',
+  //   path: '/es/index.js',
+  //   stylePath: '/dist/antd.css',
+  //   description: 'ant-design-vue 组件库',
+  //   source: 'unpkg',
+  // },
+
+  // {
+  //   name: 'naive-ui',
+  //   version: '',
+  //   path: '/es/index.js',
+  //   stylePath: '/dist/index.css',
+  //   description: 'naive ui 组件库',
+  //   source: 'unpkg',
+  // },
+  {
+    name: 'vant',
+    version: '',
+    path: '/lib/vant.es.js',
+    stylePath: '/lib/index.css',
+    description: 'vant 组件库',
+    source: 'unpkg',
+  },
+  {
+    name: '@vant/use',
+    version: '',
+    path: '/dist/index.esm.js',
+    stylePath: '',
+    description: 'vant use',
+    source: 'unpkg',
+  },
+  {
+    name: '@vant/popperjs',
+    version: '',
+    path: '/dist/esm/index.js',
+    stylePath: '',
+    description: 'vant use',
     source: 'unpkg',
   },
 ]
