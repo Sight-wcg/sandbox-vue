4dd059f926fb16fcf8c39695c2210d6c1d6af6df
sight <1453017105@qq.com>
2022-06-26 17:15:40 +0800

    feat: hash gzip

--- src/utils/encode.ts
+++ src/utils/encode.ts
@@ -1,11 +1,35 @@
+import { zlibSync, unzlibSync, strToU8, strFromU8 } from 'fflate'
+
 // prefer old unicode hacks for backward compatibility
 // https://base64.guru/developers/javascript/examples/unicode-strings
 //编码 URI to ASCII
-export function utoa(data: string): string {
-  return btoa(unescape(encodeURIComponent(data)))
-}
+// export function utoa(data: string): string {
+//   return btoa(unescape(encodeURIComponent(data)))
+// }
 
 //解码 ASCII to URI
+// export function atou(base64: string): string {
+//   return decodeURIComponent(escape(atob(base64)))
+// }
+
+export function utoa(data: string): string {
+  const buffer = strToU8(data)
+  const zipped = zlibSync(buffer, { level: 9 })
+  const binary = strFromU8(zipped, true)
+  return btoa(binary)
+}
+
 export function atou(base64: string): string {
-  return decodeURIComponent(escape(atob(base64)))
+  const binary = atob(base64)
+
+  // zlib header (x78), level 9 (xDA)
+  if (binary.startsWith('\x78\xDA')) {
+    const buffer = strToU8(binary, true)
+    const unzipped = unzlibSync(buffer)
+    return strFromU8(unzipped)
+  }
+
+  // old unicode hacks for backward compatibility
+  // https://base64.guru/developers/javascript/examples/unicode-strings
+  return decodeURIComponent(escape(binary))
 }
