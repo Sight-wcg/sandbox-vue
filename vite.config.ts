67702c7b2265230ea91afd217e2c9bd6b4ab87dc
sight <1453017105@qq.com>
2022-10-15 01:13:34 +0800

    chore: 修改 base

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ const pathSrc = path.resolve(__dirname, 'src')
 export default defineConfig(async () => {
   const repl = await getPackageInfo('vue-repl-sight')
   return {
-    base: '/sandbox-vue/',
+    base: '/',
     resolve: {
       alias: {
         '@': pathSrc,
